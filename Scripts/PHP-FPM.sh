84fd1f3f36b41c87b18f5c145bd5de30ba5d49a0
HZH <developer_hzh@hotmail.com>
2019-04-21 15:36:21 +0800

    Fix php config.

--- Scripts/PHP-FPM.sh
+++ Scripts/PHP-FPM.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 Basepath=$(cd `dirname $0`; pwd)
-apt install -y php7.2-fpm
+apt install -y php7.2-fpm php7.2-curl
 cp ${Basepath}/../Config/enable-php.conf /usr/local/nginx/conf/
 sed -i "s/;cgi.fix_pathinfo=0/cgi.fix_pathinfo=1/g" /etc/php/7.2/fpm/php.ini
-chmod -R 644 /var/run/php/
+sed -i "s/user = www-data/user = www/g" /etc/php/7.2/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = www/g" /etc/php/7.2/fpm/pool.d/www.conf
+sed -i "s/listen.owner = www-data/listen.owner = www" /etc/php/7.2/fpm/pool.d/www.conf
+sed -i "s/listen.group = www-data/listen.group = www" /etc/php/7.2/fpm/pool.d/www.conf
+service php7.2-fpm restart
