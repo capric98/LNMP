d1f7b931be07a3299f464083ca8d77414b7235ac
HZH <developer_hzh@hotmail.com>
2019-05-20 21:36:21 +0800

    Update nginx version.

--- Scripts/Nginx.sh
+++ Scripts/Nginx.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 Basepath=$(cd `dirname $0`; pwd)
+NGINX_VER="1.15.12"
 cd /root
 groupadd -r www && useradd -r -g www -s /sbin/nologin -d /usr/local/nginx -M www
 mkdir -p /usr/local/nginx
 
-apt update >> /dev/null
+apt-get update >> /dev/null
 apt install -y build-essential libpcre3 libpcre3-dev zlib1g-dev unzip git
 apt install -y sysv-rc-conf
-wget --no-check-certificate http://nginx.org/download/nginx-1.15.8.tar.gz && tar xzf nginx-1.15.8.tar.gz && rm -rf nginx-1.15.8.tar.gz
-cd nginx-1.15.8/
+wget --no-check-certificate http://nginx.org/download/nginx-${NGINX_VER}.tar.gz && tar xzf nginx-${NGINX_VER}.tar.gz && rm -rf nginx-${NGINX_VER}.tar.gz
+cd nginx-${NGINX_VER}/
 git clone https://github.com/google/ngx_brotli.git
 cd ngx_brotli && git submodule update --init && cd ../
 wget --no-check-certificate https://github.com/openssl/openssl/archive/OpenSSL_1_1_1b.zip && unzip OpenSSL_1_1_1b.zip && rm -rf OpenSSL_1_1_1b.zip
@@ -19,10 +20,13 @@ ln -s /usr/local/nginx/sbin/nginx /usr/sbin/nginx
 
 mkdir -p /usr/local/nginx/conf/vhost
 mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.bak
-rm -rf /usr/local/nginx/conf/nginx.conf
+rm -rf /usr/local/nginx/conf/nginx.conf /root/nginx-${NGINX_VER}
 cp ${Basepath}/../Config/nginx.conf /usr/local/nginx/conf/
-cp ${Basepath}/../Config/nginx /etc/init.d/ && ln -s /etc/init.d/nginx /etc/rc5.d/S01nginx && /usr/sbin/nginx
-openssl  dhparam -out /usr/local/nginx/conf/dhparam.pem 2048
+cp ${Basepath}/../Config/nginx /etc/init.d/ && ln -s /etc/init.d/nginx /etc/rc5.d/S01nginx
+cp ${Basepath}/../Config/fake.* /usr/local/nginx/conf/
+cp ${Basepath}/../Config/ForbidIP443.conf /usr/local/nginx/conf/vhost/
+#cp ${Basepath}/../Config/reverse.conf /usr/local/nginx/conf/vhost/
+openssl dhparam -dsaparam -out /usr/local/nginx/conf/dhparam.pem 4096
 sysv-rc-conf nginx on
 update-rc.d nginx defaults
-service nginx start
+service nginx restart
