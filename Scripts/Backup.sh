38407c14f597359d220ca02c017e706814491559
HZH <42015599+DeveloperHZH@users.noreply.github.com>
2019-07-07 03:12:33 +0800

    Update Backup.sh

--- Scripts/Backup.sh
+++ Scripts/Backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 list="wordpress typecho"
-PASSWD="4GfcAPs413S7V2BA"
+PASSWD="none"
 homeDir="/home/wwwroot"
 tmpDir=$(cd `dirname $0`; pwd)
 
@@ -13,8 +13,8 @@ done
 
 DATE=$(date +%Y-%m-%d)
 cd "$tmpDir"
-/usr/bin/zip -P This_is_Passw0rd -r ${DATE}-backup.zip websites mysql
+/usr/bin/zip -P This_is_Passw0rd -r ${DATE}.zip websites mysql
 rm -rf "$tmpDir"/websites "$tmpDir"/mysql
 
-/usr/bin/rclone copy -v --stats 5s "$tmpDir"/${DATE}-backup.zip GD:/WebsiteBackup/
+#/usr/bin/rclone copy -v --stats 5s "$tmpDir"/${DATE}.zip GD:/WebsiteBackup/
 
