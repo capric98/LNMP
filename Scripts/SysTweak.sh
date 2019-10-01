0e68f80f1661aa112621e5de7bb137fef8345bc7
HZH <42015599+capric98@users.noreply.github.com>
2019-10-02 03:33:00 +0800

    Update SysTweak.sh

--- Scripts/SysTweak.sh
+++ Scripts/SysTweak.sh
@@ -43,28 +43,31 @@ sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
 sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
+sed -i '/net.ipv6.conf.all.forwarding/d' /etc/sysctl.conf
 echo "fs.file-max = 1000000
 fs.inotify.max_user_instances = 8192
+net.ipv4.ip_forward = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.route.gc_timeout = 100
 net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.ip_local_port_range = 1024 65000
 net.ipv4.tcp_max_syn_backlog = 16384
 net.ipv4.tcp_max_tw_buckets = 6000
-net.ipv4.route.gc_timeout = 100
 net.ipv4.tcp_syn_retries = 1
 net.ipv4.tcp_synack_retries = 1
-net.core.somaxconn = 32768
-net.core.netdev_max_backlog = 32768
-net.ipv4.tcp_timestamps = 0
 net.ipv4.tcp_max_orphans = 32768
-net.core.rmem_max = 12582912
-net.core.wmem_max = 12582912
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_rmem = 4096    524000  67110000
 net.ipv4.tcp_wmem = 4096    524000  67110000
-net.ipv4.tcp_mem = 1541547 2055396 3083094
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.conf
+net.ipv4.tcp_mem  = 1541547 2055396 3083094
+net.ipv6.conf.all.forwarding=1
+net.core.netdev_max_backlog = 32768
+net.core.rmem_max = 12582912
+net.core.somaxconn = 32768
+net.core.wmem_max = 12582912
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr" >>/etc/sysctl.conf
 sysctl -p
 
 apt-get update >> /dev/null
